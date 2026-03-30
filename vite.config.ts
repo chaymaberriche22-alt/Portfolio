59af434171c37061876a24372e06a26b7b12d4e0
Chayma Berriche <chaymaberriche22@gmail.com>
2026-03-30 17:24:11 +0100

    Updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite'
 
-// GitHub Pages often serves your site from a sub-path:
-// https://<user>.github.io/<repo>/
-//
-// Using a relative base ensures assets work both locally and on Pages.
+
 export default defineConfig({
   base: './',
 })
